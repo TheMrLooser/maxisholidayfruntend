9e7c658206f50d3b079b51cdf4425982d0e5d5d5
Ankush Kumar <ankushsoni7171@gmail.com>
2022-12-04 16:23:40 +0530

    update-hostname

--- src/AWS_server_IP.js
+++ src/AWS_server_IP.js
@@ -1,3 +1,3 @@
-// export const HOST_NAME = 'http://35.77.189.124'
-export const HOST_NAME = 'https://maxis-holiday.herokuapp.com' 
+export const HOST_NAME = 'http://maxisholidayserver.cf'
+// export const HOST_NAME = 'https://maxis-holiday.herokuapp.com' 
 // export const HOST_NAME = 'http://localhost:80'
