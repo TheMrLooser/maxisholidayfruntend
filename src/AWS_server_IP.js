62308c178fa1436b43f4cb54829cfe5d167a67fa
Ankush Kumar <ankushsoni7171@gmail.com>
2022-12-07 18:42:28 +0530

    updated

--- src/AWS_server_IP.js
+++ src/AWS_server_IP.js
@@ -1,3 +1,4 @@
-export const HOST_NAME = 'http://maxisholidayserver.cf'
+// export const HOST_NAME = 'http://maxisholidayserver.cf'
 // export const HOST_NAME = 'https://maxis-holiday.herokuapp.com' 
 // export const HOST_NAME = 'http://localhost:80'
+export const HOST_NAME = 'https://maxisholidayserver.onrender.com'
